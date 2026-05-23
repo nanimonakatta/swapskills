92a597e254bd7c367d65b2d89ecfa296cd29d972
priyansh <sahpreeyansh@gmail.com>
2026-05-23 19:48:21 +0530

    file upload works now

--- server/src/app.js
+++ server/src/app.js
@@ -16,8 +16,8 @@ app.use(express.static("../public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
-app.use('/api/v1/user/', userRouter);
-app.use('/api/v1/video/', videoRouter)
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/video', videoRouter);
 
 app.use(errorHandler);
 
